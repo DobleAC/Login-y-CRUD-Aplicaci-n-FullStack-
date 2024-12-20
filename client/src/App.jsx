1b63deff7ae23565771bc5c006827c613a1ab7e1
votaredes@gmail.com <votaredes@gmail.com>
2024-12-19 20:50:13 -0700

    Validaciones del backend incorporadas en el frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,23 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
 
 function App(){
   return (
-    <BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
       <Routes>
         <Route path='/' element={<h1>Home Page</h1>} />
-        <Route path='/login' element={<h1>Login</h1>} />
-        <Route path='/register' element={<h1>Register</h1>} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
         <Route path='/tasks' element={<h1>Tasks page</h1>} />
         <Route path='/add-task' element={<h1>new task</h1>} />
         <Route path='/tasks/:id' element={<h1>update task</h1>} />
         <Route path='/profile' element={<h1>Profile</h1>} />
       </Routes>
     </BrowserRouter>
+    </AuthProvider>
   )
 }
 export default App;

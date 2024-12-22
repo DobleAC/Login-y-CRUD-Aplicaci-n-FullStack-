58eb27ba82b75248004878340e3f558901d81f2c
votaredes@gmail.com <votaredes@gmail.com>
2024-12-21 19:08:41 -0700

    Finalización de rutas protegidas.

---
+++ client/src/pages/TasksPage.jsx
@@ -0,0 +1,11 @@
+import {useAuth} from "../context/AuthContext";
+
+function TasksPage(){
+    const {user} = useAuth();
+
+    
+    return  <div>Tasks Page</div>;
+    
+}
+
+export default TasksPage;

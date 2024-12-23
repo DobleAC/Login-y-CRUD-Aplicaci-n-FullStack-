166b0d8a806070250028c80f6ecb226ace80dbea
votaredes@gmail.com <votaredes@gmail.com>
2024-12-22 23:51:44 -0700

    Petición eliminar tareas finalizado, editar tarea solo empezado (falta configurarlo para que edite las tareas selecionadas

--- client/src/context/TasksContext.jsx
+++ client/src/context/TasksContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import {createTaskRequest, getTasksRequest} from '../api/tasks';
+import {createTaskRequest, getTasksRequest, deleteTaskRequest} from '../api/tasks';
 
 const TaskContext = createContext();
 
@@ -33,12 +33,23 @@ export function TaskProvider({ children }) {
         console.log(res);
 
     }
+    const deleteTask = async (id) =>{
+        try {
+            const res = await deleteTaskRequest(id);
+            if(res.status === 204) setTasks(tasks.filter((task) => task._id !== id));
+            console.log(res);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     return (
         <TaskContext.Provider value={{
             tasks,
             createTask,
             getTasks,
+            deleteTask,
         }}>
             {children}
         </TaskContext.Provider>
